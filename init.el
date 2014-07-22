7c94f26ded3c87db5809a88796197d64bdb589da
Tosh Lyons <tosh.lyons@gmail.com>
2014-07-22 17:28:31 -0500

    Initial commit
    Basic working structure of org based init

---
+++ init.el
@@ -0,0 +1,10 @@
+;; This sets up the load path so that we can override it
+(package-initialize nil)
+;; Override the packages with the git version of Org
+(add-to-list 'load-path "~/elisp/org-mode/lisp")
+(add-to-list 'load-path "~/elisp/org-mode/contrib/lisp")
+;; Load the rest of the packages
+(package-initialize t)
+(require 'org)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config/tosh.org"))
