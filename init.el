948e7004ab373537cf07e0ceede1bed612fb102b
Tosh Lyons <tosh.lyons@gmail.com>
2014-08-14 19:02:09 -0500

    add private; add switch-window; jabber, org tweaks
    and a bunch of accidental indentation changes

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 ;; This sets up the load path so that we can override it
 (package-initialize nil)
 ;; Override the packages with the git version of Org
-(add-to-list 'load-path "~/elisp/org-mode/lisp")
-(add-to-list 'load-path "~/elisp/org-mode/contrib/lisp")
+(add-to-list 'load-path "~/.emacs.d/elisp/org-mode/lisp")
+(add-to-list 'load-path "~/.emacs.d/elisp/org-mode/contrib/lisp" t)
 ;; Load the rest of the packages
 (package-initialize t)
 (require 'org)
